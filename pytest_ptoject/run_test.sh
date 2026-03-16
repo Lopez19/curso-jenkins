01819dd8e3fb9774a0f860f9c5196532ab9b2e92
Horacio Lopez Moreno <horaciolopm_2001@hotmail.com>
2026-03-16 10:41:14 -0500

    feat: eliminar archivos y configuraciones del proyecto pytest-ptoject

--- pytest_ptoject/run_test.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-echo "Install the dependencies from pyproject.toml"
-pip install -e .[dev]
-
-echo "Run the tests"
-pytest tests/ --junit-xml=reports/test-results.xml --html=reports/test-report.html --self-contained-html
-
-echo "Pruebas finalizadas resultados en reports/"
